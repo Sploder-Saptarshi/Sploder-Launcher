f8042e2c638176e0a826d8af24cdc5daf30e90b6
Sploder-Saptarshi <85979829+Sploder-Saptarshi@users.noreply.github.com>
2022-09-11 14:12:19 +0530

    Upload files

---
+++ build_win32.bat
@@ -0,0 +1,3 @@
+call npm run dist-32bit
+pause
+Echo "Command Finished Successfully!"
---
+++ src/main/index.js
@@ -0,0 +1,155 @@
+const { app, BrowserWindow } = require("electron");
+const path = require("path");
+const fs = require("fs");
+const { Menu } = require('electron')
+const isMac = process.platform === 'darwin'
+const template = [
+  // { role: 'appMenu' }
+  ...(isMac ? [{
+    label: app.name,
+    submenu: [
+      { role: 'about' },
+      { type: 'separator' },
+      { role: 'services' },
+      { type: 'separator' },
+      { role: 'hide' },
+      { role: 'hideOthers' },
+      { role: 'unhide' },
+      { type: 'separator' },
+      { role: 'quit' }
+    ]
+  }] : []),
+  // { role: 'fileMenu' }
+  {
+    label: 'File',
+    submenu: [
+      isMac ? { role: 'close' } : { role: 'quit' }
+    ]
+  },
+  // { role: 'editMenu' }
+  {
+    label: 'Edit',
+    submenu: [
+      { role: 'undo' },
+      { role: 'redo' },
+      { type: 'separator' },
+      { role: 'cut' },
+      { role: 'copy' },
+      { role: 'paste' },
+      ...(isMac ? [
+        { role: 'pasteAndMatchStyle' },
+        { role: 'delete' },
+        { role: 'selectAll' },
+        { type: 'separator' },
+        {
+          label: 'Speech',
+          submenu: [
+            { role: 'startSpeaking' },
+            { role: 'stopSpeaking' }
+          ]
+        }
+      ] : [
+        { role: 'delete' },
+        { type: 'separator' },
+        { role: 'selectAll' }
+      ])
+    ]
+  },
+  // { role: 'viewMenu' }
+  {
+    label: 'View',
+    submenu: [
+      { role: 'reload' },
+      { role: 'forceReload' },
+      { role: 'toggleDevTools' },
+      { type: 'separator' },
+      { role: 'resetZoom' },
+      { role: 'zoomIn' },
+      { role: 'zoomOut' },
+      { type: 'separator' },
+      { role: 'togglefullscreen' }
+    ]
+  },
+]
+const menu = Menu.buildFromTemplate(template)
+
+let win;
+let pluginName;
+
+switch (process.platform) {
+  case "win32":
+    pluginName = process.arch == 'x64' ? 'x64/pepflashplayer.dll' : 'x32/pepflashplayer32.dll';
+    break;
+   case 'linux':
+    pluginName = 'libpepflashplayer.so'
+    break;
+  default:
+    pluginName = 'x64/pepflashplayer.dll';
+    break;
+}
+
+app.commandLine.appendSwitch(
+  "ppapi-flash-path",
+  path.join(__dirname + pluginName)
+);
+app.commandLine.appendSwitch("ppapi-flash-version", "32.0.0.371");
+
+
+function createWindow() {
+	  Menu.setApplicationMenu(menu)
+  win = new BrowserWindow({
+    webPreferences: {
+      nodeIntegration: false,
+      contextIsolation: false,
+      plugins: true,
+    },
+  });
+
+
+  win.maximize();
+win.loadURL("https://sploder.us.to/play.php");}
+
+app.whenReady().then(() => {
+  createWindow();
+
+  app.on("activate", function () {
+    if (BrowserWindow.getAllWindows().length === 0) createWindow();
+  });
+});
+
+app.on("window-all-closed", function () {
+  if (process.platform !== "darwin") app.quit();
+});
+
+
+const clientId = '915116210570539058';
+const rpc = new DiscordRPC.Client({ transport: 'ipc' });
+const startTimestamp = new Date();
+
+async function setActivity() {
+  if (!rpc || !win) {
+    return;
+  }
+  const boops = await win.webContents.executeJavaScript('window.boops');
+
+  // You'll need to have snek_large and snek_small assets uploaded to
+  // https://discord.com/developers/applications/<application_id>/rich-presence/assets
+  rpc.setActivity({
+    details: `${boops}`,
+    startTimestamp,
+    largeImageKey: 'icon',
+    largeImageText: `${boops}`
+  });
+}
+
+rpc.on('ready', () => {
+  setActivity();
+
+  // activity can only be set every 15 seconds
+  setInterval(() => {
+    setActivity();
+  }, 15e3);
+});
+
+rpc.login({ clientId }).catch(console.error);
+
